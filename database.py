3a9ad1fb9410d0ca6ee049e5d45ec3f242e2ccbd
JackOMan69 <gypg5872@gmail.com>
2022-07-31 10:53:11 +0300

    Solving all todos

--- database.py
+++ database.py
@@ -1,16 +1,22 @@
 import sqlite3
 
+with sqlite3.connect("database/notes.db") as db:
+    cursor = db.cursor()
+    initialization_query = """CREATE TABLE IF NOT EXISTS notes(note_id INT PRIMARY KEY, note_name TEXT, note_date TEXT, note_description TEXT, tags TEXT)"""
+    cursor.execute(initialization_query)
 
-def add_note(note_name, note_date, note_description, user_id, tags):
-    with sqlite3.connect("notes_api_database/notes_db.db") as db:
+
+def add_note(note_name, note_description, tags):
+    with sqlite3.connect("database/notes.db") as db:
         cursor = db.cursor()
-        query = """ INSERT INTO notes(note_name, note_date, note_description, user_id, tags) 
-                    VALUES("{}", "{}", "{}", "{}", "{}")""".format(note_name, note_date, note_description, user_id, tags)
-        cursor.execute(query)
+        cursor.executescript(
+            """SELECT datetime("now","localtime");
+                INSERT INTO notes(note_name, note_date, note_description, tags) VALUES("{}", datetime("now","localtime"), "{}", "{}")""".format(note_name, note_description, tags)
+        )
 
 
 def delete_note(note_id):
-    with sqlite3.connect("notes_api_database/notes_db.db") as db:
+    with sqlite3.connect("database/notes.db") as db:
         cursor = db.cursor()
         delete_query = """ DELETE FROM notes WHERE note_id = {}""".format(
             note_id)
@@ -18,7 +24,7 @@ def delete_note(note_id):
 
 
 def check_deleted_note(note_id):
-    with sqlite3.connect("notes_api_database/notes_db.db") as db:
+    with sqlite3.connect("database/notes.db") as db:
         cursor = db.cursor()
         check_deleted_id = cursor.execute(
             """SELECT note_id FROM notes WHERE note_id = {}""".format(note_id))
@@ -28,21 +34,20 @@ def check_deleted_note(note_id):
             return True
 
 
-def change_note(note_id, note_name, note_date, note_description, user_id, tags):
-    with sqlite3.connect("notes_api_database/notes_db.db") as db:
+def change_note(note_id, note_name, note_description, tags):
+    with sqlite3.connect("database/notes.db") as db:
         cursor = db.cursor()
         change_note_name_query = """UPDATE notes 
                                     SET note_name = "{}", 
-                                    note_date = "{}", 
-                                    note_description = "{}", 
-                                    user_id = {}, 
+                                    note_description = "{}",  
                                     tags = "{}"
-                                    WHERE note_id = {} """.format(note_name, note_date, note_description, user_id, tags, note_id)
-        cursor.execute(change_note_name_query)
+                                    WHERE note_id = {} """.format(note_name, note_description, tags, note_id)
+        changed_note = cursor.execute(change_note_name_query)
+        return changed_note
 
 
 def get_all_notes():
-    with sqlite3.connect("notes_api_database/notes_db.db") as db:
+    with sqlite3.connect("database/notes.db") as db:
         cursor = db.cursor()
         cursor.execute("""SELECT * FROM notes""")
         get_all_notes_query = cursor.fetchall()
@@ -52,14 +57,13 @@ def get_all_notes():
                                   "note_name": row[1],
                                   "note_date": row[2],
                                   "note_description": row[3],
-                                  "user_id": row[4],
-                                  "tags": row[5]}
+                                  "tags": row[4]}
             result_dict.append(get_all_notes_dict)
         return result_dict
 
 
 def get_note_by_id(note_id):
-    with sqlite3.connect("notes_api_database/notes_db.db") as db:
+    with sqlite3.connect("database/notes.db") as db:
         cursor = db.cursor()
         get_note_by_id_query = cursor.execute(
             """SELECT * FROM notes WHERE note_id = {}""".format(note_id))
@@ -68,13 +72,12 @@ def get_note_by_id(note_id):
                                 "note_name": row[1],
                                 "note_date": row[2],
                                 "note_description": row[3],
-                                "user_id": row[4],
-                                "tags": row[5]}
+                                "tags": row[4]}
         return single_note_dict
 
 
 def get_id_note():
-    with sqlite3.connect("notes_api_database/notes_db.db") as db:
+    with sqlite3.connect("database/notes.db") as db:
         cursor = db.cursor()
         get_id_note_query = cursor.execute(
             """SELECT note_id FROM notes ORDER BY note_id DESC LIMIT 1""")
--- main.py
+++ main.py
@@ -1,103 +1,60 @@
 import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
-from notes_db_py import add_note, delete_note, change_note, get_all_notes, get_note_by_id, get_id_note, check_deleted_note
+from database import add_note, delete_note, change_note, get_all_notes, get_note_by_id, check_deleted_note, get_id_note
 
 app = FastAPI()
 
-'''
-Используй чаще константы, например
+
+class Note(BaseModel):
+    note_name: str
+    note_description: str
+    tags: list | None = None
+
 
 BASE_URL = "/api/notes/"
+
 
 @app.get(BASE_URL)
 async def get_notes():
     return get_all_notes()
 
+
 @app.get(BASE_URL + "{note_id}")
-async def get_note(note_id):
-    return get_note_by_id(note_id)
-
-Старайся чтобы код не повторялся
-'''
-
-class Note(BaseModel):
-    note_name: str
-    note_date: str # Проверирь есть ли в питоне формат даты или в этой хрени, как бы хотелось бы чтобы это была не строка а дата
-    note_description: str
-    user_id: int # Че за user_id?)
-    tags: str | None = None
-
-
-@app.get("/api/notes/")
-async def get_notes():
-    return get_all_notes()
-
-
-@app.get("/api/notes/{note_id}")
-async def get_note(note_id):
+async def get_note(note_id: int):
     return get_note_by_id(note_id)
 
 
-@app.post("/api/notes/")
+@app.post(BASE_URL)
 async def create_note(note: Note):
     '''
-    Почему тут такой велосипед?
-    Зачем нужна функция get_id_note, если можно возвращать 
-    note_id с add_note???
-
-    должно быть что-то типо
-    new_note = add_note(note.note_name, note.note_date,
-             note.note_description, note.user_id, note.tags)
-    
+    Понимаешь блин блять,
+    Тут не получится просто взять из note нужную нам колоночку
+    Нам нужно выполнение отдельной функции под это дело, чтобы не было никаких ошибок
+    У меня не получилось твоим решением, потому что выдавало ошибку, оставлю так
+    '''
+    add_note(note.note_name, note.note_description, note.tags)
     result = {
-        result: 'OK',
-        data: {
-            note_id: new_note.note_id
-        }
-    } или вместо возвращение note_id можно возвращать всю заметку
-
-    result = {
-        result: 'OK',
-        data: new_note
+        "result": "Successful!",
+        "Your note": get_id_note()
     }
-    
     return result
-    '''
-    add_note(note.note_name, note.note_date,
-             note.note_description, note.user_id, note.tags)
-    last_note_id = {"The last note's id": get_id_note()}
-    return last_note_id
 
 
-@app.put("/api/notes/")
-async def note_into_db(note: Note, note_id):
-    '''
-    Тоже самое. 
-    Почему мы возвращем то что отправил фронтенд.
-
-    функция change_note должна возвращать нам заметку полность,
-    вместе с note_id и всех фигней
-    '''
-    change_note(note_id, note.note_name, note.note_date,
-                note.note_description, note.user_id, note.tags)
-    return note
+@app.put(BASE_URL)
+async def note_into_db(note: Note, note_id: int):
+    change_note(note_id, note.note_name, note.note_description, note.tags)
+    return get_note_by_id(note_id)
 
 
-@app.delete("/api/notes/{note_id}")
+@app.delete(BASE_URL + "{note_id}")
 async def delete_from_db(note_id: int):
-    '''
-    Никаких wow и Successful. Я про тебе это написал в туду
-
     if check_deleted_note(note_id):
-        return "Wow! This note has already been deleted or hasn't exist at all!"
-    Можно пользователю не возвращать такую фигню, а постоянно слать 'OK' типо даэе если он прислал id, которого не существует, Всегда ок
-    '''
-    if check_deleted_note(note_id):
-        return "Wow! This note has already been deleted or hasn't exist at all!"
+        return "This note has been already deleted or doesn't exist at all"
     else:
         delete_note(note_id)
         return "Successful!"
+
 
 # https://fastapi.tiangolo.com/tutorial/debugging/
 if __name__ == "__main__":
